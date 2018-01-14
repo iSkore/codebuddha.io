5b180020f177b7d1d5705ec1ed95f49a29beb5d1
iskore <iSkore@users.noreply.github.com>
2018-01-14 15:28:50 -0500

    adding config for apps

--- app/js/config.js
+++ app/js/config.js
@@ -1,21 +1,11 @@
 'use strict';
 
 module.exports = {
-    region: 'us-east-1',
-    IdentityPoolId: 'us-east-1:263df2e9-293a-4a03-8597-7f4905c379db',
-    UserPoolId: 'us-east-1_BeIKCdmAP',
-    ClientId: '4fbkc8ett5ea1lth9nu05titgq',
-    Paranoia: 7,
-    endpoints: {
-        auth: 'cognito-idp.us-east-1.amazonaws.com/us-east-1_BeIKCdmAP',
-        S3: 'https://s3-accelerate.amazonaws.com/'
-    },
-    S3: {
-        Bucket: 'files-codebuddha-io',
-        Delimiter: '/'
-    },
-    DDB: {
-        SUB_TZ: 'awsmanager-SUB-TZ',
-        AUD_SUB: 'awsmanager-AUD-SUB'
-    }
+    applications: [
+        {
+            title: 'Byte Converter',
+            desc: 'Convert Bytes, Kilobytes, Megabytes, Gigabytes, Terabytes',
+            route: './modules/byteConverter/'
+        }
+    ]
 };
